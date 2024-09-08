3c228ef79d0386b11ea44041988b2432a7f01d9d
Truman <ljx2685222739@gmail.com>
2024-09-09 07:43:22 +0800

    优化配置和添加自定义错误页面

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    optimizeCss: true,
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.optimization.splitChunks.cacheGroups = {
