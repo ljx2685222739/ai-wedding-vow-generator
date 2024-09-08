31b3e1682c075b426708aa44f84f08f898911222
Truman <ljx2685222739@gmail.com>
2024-09-08 23:01:03 +0800

    1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 移除 output: 'export'
+  output: 'standalone',
+  experimental: {
+    optimizeCss: true, // 启用 CSS 优化
+  },
+  webpack: (config, { isServer }) => {
+    // 只在客户端构建中应用这些优化
+    if (!isServer) {
+      config.optimization.splitChunks.cacheGroups = {
+        ...config.optimization.splitChunks.cacheGroups,
+        commons: {
+          name: 'commons',
+          chunks: 'all',
+          minChunks: 2,
+        },
+      };
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
