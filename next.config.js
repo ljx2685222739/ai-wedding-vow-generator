2d2b22ec757682cf4efb3eaa13c153bfba6dea38
Truman <ljx2685222739@gmail.com>
2024-09-08 23:04:15 +0800

    1

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  experimental: {
-    optimizeCss: true, // 启用 CSS 优化
-  },
   webpack: (config, { isServer }) => {
-    // 只在客户端构建中应用这些优化
     if (!isServer) {
       config.optimization.splitChunks.cacheGroups = {
         ...config.optimization.splitChunks.cacheGroups,
